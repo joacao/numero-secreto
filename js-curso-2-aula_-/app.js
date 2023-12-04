f72853046e84802e6c8929e70366549a0bbc6524
joacao <henriquejoaquim.hd@gmail.com>
2023-12-04 05:24:22 -0300

    Alterando limite de 10 para 100

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,6 +1,6 @@
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -13,7 +13,7 @@ function exibirTextoNaTela(tag, texto){
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Selecione um número entre 1 e 10');
+exibirTextoNaTela('p', 'Selecione um número entre 1 e 100');
 
 function verificarChute(){
     let chute = document.querySelector('input').value;
